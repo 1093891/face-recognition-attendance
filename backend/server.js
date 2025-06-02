6906f0269019079941b2b92e6b026200e65a4286
Yisak Metaferiya <1093891@students.adu.ac.ae>
2025-06-02 22:12:34 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // PostgreSQL Configuration for Render
 const pool = new Pool({
